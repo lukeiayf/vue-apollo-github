51e8fc0b656c328aad18c777aec7689629ad1b2c
Lucas Silva <lucas.silva@tsaaviacao.com.br>
2022-07-14 17:05:37 -0300

    first working repo with repositories queries

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
   <h1
     class="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10 max-w-6xl mx-auto"
   >
-    Find
+    Procure
     <span
       class="bg-clip-text text-transparent bg-gradient-to-r from-green-600 via-green-500 to-green-400"
-      >GitHub</span
+      >Usuários do Github</span
     >
-    Repositories:
   </h1>
   <SearchBar @search="search" />
   <RepositoryList :search-options="searchOptions" />
